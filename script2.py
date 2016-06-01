36f19063cb78ec7612aa77e2441a867a37d43c33
DenisHawkins <denis.hawkins@anu.edu.au>
2016-06-01 10:23:30 +1000

    SCRIPT WORK 1 DAY3

---
+++ script2.py
@@ -0,0 +1,41 @@
+
+#!/usr/bin/env python
+# Module import sys       
+import sys
+
+
+
+#float - number allow for Decimal places
+#cel to Far
+
+
+#print(float(num))
+#type(float(num))
+#CalARG = sys.argv[1]
+#result = CalARG *  1.8 + 32
+ 
+
+print('hello')
+print('These are the arguments: ',sys.argv)
+print('This is the first arguments: ',sys.argv[0])
+print('This is the first arguments: ',sys.argv[1])
+print('COnvert to Cel from Far: ',result)
+
+#------------------ FINAL METHOD
+
+def celcius_to_fahr(temp_c):
+	temp_f=temp_c* (9.0/5.0) + 32
+	return temp_f
+
+celc=float(sys.argv[1])
+print(celsious_to_fahr(temp_f)
+
+#test if the text works
+
+try:
+	cels=float(sys.argv[1])
+	print(celsius_to_fahr(cels))
+
+printer("try again")
+
+
